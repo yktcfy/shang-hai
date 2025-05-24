25c9c7b458d9f4af9ac75bf2ba12f6da14fd88da
yktcfy <zjyktcfy@qq.com>
2025-05-24 11:52:52 +0800

    Update and rename zubo2.sh to CaijiShanghaiip.sh

--- CaijiShanghaiip.sh
+++ CaijiShanghaiip.sh
@@ -3,7 +3,7 @@ if [ $# -eq 0 ]; then
   echo "开始测试······"
   echo "在5秒内输入1~4可选择城市"
   echo "请选择城市："
-  echo "1. 上海电信"
+  echo "1. 上海（Shanghai_dianxin）"
   read -t 5 -p "超时未输入,将按默认设置测试" city_choice
 
   if [ -z "$city_choice" ]; then
@@ -17,7 +17,7 @@ fi
 # 设置城市和相应的stream
 case $city_choice in
     1)
-        city="上海电信"
+        city="Shanghai_dianxin"
         stream="rtp/239.45.3.237:5140"
         ;;
     0)
